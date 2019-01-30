43f11aa305fc9f65b583ff5f3cdbb8930b208b6c
007kartik <rohit.audrey@gmail.com>
2019-01-30 14:26:00 +0530

    made the web app smoother

--- models/registerM.js
+++ models/registerM.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 //creating our login Schema
 const registerSc = new Schema({
   name: {
@@ -10,7 +15,10 @@ const registerSc = new Schema({
 
   email: {
     type:String,
-    required: ['true', 'email field is required']
+    required: ['true', 'email field is required'],
+    validate: [validateEmail, 'Please fill a valid email address'],
+         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    index: {unique: true, dropDups: true}
   },
   password: {
     type:String,
